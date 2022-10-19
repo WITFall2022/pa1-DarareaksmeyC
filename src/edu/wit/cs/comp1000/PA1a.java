019454eaf1d6c756ecbc71a04f62040ea277e9d1
316154 <316154@664-161453-lab.schools.mybps.org>
2022-10-19 10:41:11 -0400

    Hi.I have added some code to this program

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,10 +1,31 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int yards;
+		System.out.printf("Enter number of yards: ");
+		yards = input.nextInt();
+		
+		int feet;
+		System.out.printf("Enter number of feet: ");
+		feet = input.nextInt();
+		
+		int inches;
+		System.out.printf("Enter number of inches: ");
+		inches = input.nextInt();
+		
+		int totalInches = (yards * 36) + (feet * 12) + inches;
+		
+		System.out.printf("Total number of inches: %d", totalInches);
+		
 		// TODO: write your code here
 	}
 
